7e6b3aac3f3a1441cc7bb70caed873c0927f3e71
Nguyễn Đức Đạt <nddat2002@gmail.com>
2025-06-10 21:09:27 +0700

    update TestNGTestRunner.java

--- SeleniumFramworkDesign/src/test/java/cucumber/TestNGTestRunner.java
+++ SeleniumFramworkDesign/src/test/java/cucumber/TestNGTestRunner.java
@@ -3,6 +3,7 @@ package cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
+//cucumber -> testng , junit
 @CucumberOptions(features = "src/test/java/cucumber", glue = "datnd.stepDefinitions", monochrome = true, tags = "@Regression", plugin = {
 		"html:target/cucumber.html" })
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
